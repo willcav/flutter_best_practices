4573f2f02b5918c596f7f3769db6b87a281d93bf
Will Cavenagli <willian.cavenagli@usemobile.xyz>
2022-01-29 10:12:49 -0300

    🧩: Add RemoteGetUser to Data Layer

---
+++ lib/src/data/usecases/remote_get_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_clean_arch_error_handling/src/domain/entities/user_entity.dart';
+import 'package:flutter_clean_arch_error_handling/src/domain/usecases/get_user_usecase.dart';
+
+class RemoteGetUser extends GetUserUseCase {
+  final mockUser = const UserEntity(name: 'John Doe');
+
+  @override
+  Future<UserEntity> call() async {
+    try {
+      return mockUser;
+    } catch (e) {
+      throw Exception();
+    }
+  }
+}
